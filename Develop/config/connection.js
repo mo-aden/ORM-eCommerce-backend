40b5ffb0d29c76138cf67b3f9f12bfa8f4f019ce
mo-aden <mohamedaden17@outlook.com>
2022-12-17 23:17:10 -0700

    added columns to all the db models

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
+console.log(process.env);
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
+      host: "127.0.0.1",
+      dialect: "mysql",
       dialectOptions: {
         decimalNumbers: true,
       },
--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,19 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
